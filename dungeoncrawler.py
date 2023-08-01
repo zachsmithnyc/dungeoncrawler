b56e6138a46dfd97cf1722baa9f6144f91de940b
zachsmithnyc <zachsmithnyc@gmail.com>
2023-08-01 11:01:42 -0400

    Day1

---
+++ dungeoncrawler.py
@@ -0,0 +1,30 @@
+class Character:
+  # creates a new character
+  def __init__(self, name, job, level = 1):
+    self.name = name 
+    self.job = job
+    self.level = level
+    
+    if self.job = 'warrior':
+      self.hp = level * 10
+      self.max_hp = self.hp
+      self.attack = level * 5
+    if self.job = 'thief':
+      self.hp = level * 7
+      self.max_hp = self.hp
+      self.attack = level * 3
+    if self.job = 'healer':
+      self.hp = level * 5
+      self.max_hp = self.hp
+      self.attack = level * 1
+  
+  def __repr__(self):
+    return f"{self.name} is a level {self.level} {self.job} with {self.hp} hit points."
+
+  # method for attacking 
+  def attack (self, monster):
+class Monster:
+  # creates monsters for characters to fight
+
+  def __init__(self, type):
+    self.type = type    
