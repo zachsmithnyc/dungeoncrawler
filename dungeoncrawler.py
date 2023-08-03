07aece85b6cd8de22f08b3049b5fc1e1117b7733
zachsmithnyc <zachsmithnyc@gmail.com>
2023-08-03 13:01:53 -0400

    day3

--- dungeoncrawler.py
+++ dungeoncrawler.py
@@ -7,25 +7,40 @@ class Character:
     self.is_knocked_out = False
     
     if self.job == 'warrior':
-      self.hp = level * 10
+      self.hp = round(level * 10)
       self.max_hp = self.hp
-      self.power = level * 5
-    elif self.job == 'thief':
-      self.hp = level * 7
+      self.power = round(level * 5)
+      self.heal_power = 0
+    if self.job == 'thief':
+      self.hp = round(level * 7)
       self.max_hp = self.hp
-      self.power = level * 3
-    else:
-      self.hp = level * 5
+      self.power = round(level * 3)
+      self.heal_power = 0
+    if self.job == 'cleric':
+      self.hp = round(level * 5)
       self.max_hp = self.hp
-      self.power = level * 1
+      self.power = round(level * 1)
+      self.heal_power = 5
   
   def __repr__(self):
-    return f"{self.name} is a level {self.level} {self.job} with {self.hp} hit points."
+    return f"{self.name} is a level {self.level} {self.job} with {self.hp} hit points and {self.power} power."
   
+  # revive method
+  def revive(self):
+    self.is_knocked_out = False
+    if self.hp == 0:
+      self.hp = 1
+    print(f"{self.name} was revived!")
+
   # method for knock out
   def knock_out(self):
     # sets knock out status to true 
     self.is_knocked_out = True
+
+    if self.hp != 0:
+      self.hp = 0
+    
+    print(f"{self.name} was knocked out!")
 
   # method for losing health
   def lose_hp(self, amount):
@@ -33,6 +48,8 @@ class Character:
     if self.hp <= 0:
       self.hp = 0
       self.knock_out()
+    else:
+      print(f"{self.name} hp was reduced to {self.hp}")
 
   # method for gaining health
   def gain_hp(self, amount):
@@ -40,18 +57,21 @@ class Character:
 
     #checks knock our status and flips it to false on heal 
     if self.is_knocked_out == True:
-      self.is_knocked_out == False
-    if self.hp >= self.max_hp:
-      self.hp = self.max_hp
+      self.revive()
 
-    print(f"{self.name} was healed {amount} points!")
-     
-
-  # method for stealing 
-  def heal(self, target):
-    pass
+    print(f"{self.name} now has {self.hp} hit points!")
+  
+  # method for attacking
+  def attack(self, target):
+    print(f"{self.name} attacked {target.name}!")
+    target.lose_hp(self.power)
 
   # method for healing 
+  def heal(self, target):
+    print(f"{self.name} heals {target.name}")
+    target.gain_hp(self.heal_power)
+
+  # method for stealing 
   def steal(self, target):
     pass
 
@@ -63,27 +83,36 @@ class Monster:
     
     if self.name == 'ogre':
       self.power = 5
-    elif self.name == 'wolf':
-      self.power = 3
-    else:
-      self.power = 1
-
-    if self.name == 'ogre':
       self.item = 'potion'
     if self.name == 'wolf':
+      self.power = 3
       self.item == 'wolf teeth'
-    else:
+    if self.name == 'fairy':
+      self.power = 1
       self.item == 'antidote'
 
   #method for attacking 
   def attack(self, target):
-    
+    target.lose_hp(self.power)
 
   #method for aoe attack
-  def lose_health(self):
-    pass 
+  def poison(self, target):
+    pass
 
   #method for healing
   def heal(self, target):
     pass
+
+
+warrior = Character('Samson', 'warrior')
+thief = Character('Clyde', 'thief')
+cleric = Character('Sheila', 'cleric')
+
+print(warrior.__repr__())
+print(thief.__repr__())
+print(cleric.__repr__())
+warrior.attack(thief)
+warrior.attack(thief)
+cleric.heal(thief)
+
     
